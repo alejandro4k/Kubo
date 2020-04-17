763d136bd9c6b05225856bea519e058c99fcbfe6
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-04-17 00:39:46 -0500

    Updates

---
+++ precache-manifest.e1e5ac205c7ca80fae1e12de90cf88fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d817212df4b4647d56c6c28cc189e0b9",
+    "url": "/Kubo/index.html"
+  },
+  {
+    "revision": "12790f2f0e1f21fa4ee1",
+    "url": "/Kubo/static/css/main.d63e5908.chunk.css"
+  },
+  {
+    "revision": "13114328f99992e4f3ce",
+    "url": "/Kubo/static/js/2.ed4145f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kubo/static/js/2.ed4145f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12790f2f0e1f21fa4ee1",
+    "url": "/Kubo/static/js/main.e2b4dd3e.chunk.js"
+  },
+  {
+    "revision": "c2a80852911ea232fe61",
+    "url": "/Kubo/static/js/runtime-main.4afda581.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kubo/static/media/logo.5d5d9eef.svg"
+  }
+]);
